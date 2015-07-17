f53235b333bac01f416789ee3c818a3755308844
Bartosz Kędziora <bartosz.kedziora@stazysta.comarch.pl>
2015-07-17 15:55:57 +0200

    Tasks are now shown by AngularJS directive

--- js/app.js
+++ js/app.js
@@ -4,12 +4,26 @@
 	function() {
 		var testTasks = [
 			new Task("Doing stuff", "Various stuff"),
-			new Task("Doing other stuff", "Various stuff")
+			new Task("Doing other stuff", "Various stuff"),
+			new Task("Oh whatever", "Other stuff"),
+			new Task("Blah blah", "Other stuff"),
+			new Task("Job done!", "Other stuff")
 		];
+		
+		testTasks[2].Status = Task.enumStatus.PLANNED;
+		testTasks[3].Status = Task.enumStatus.IN_PROGRESS;
+		testTasks[4].Status = Task.enumStatus.DONE;
 
 		var app = angular.module('TaskList', []);
 		app.controller('TaskController', function() {
 			this.tasks = testTasks;
+		});
+		
+		app.directive('task', function() {
+			return {
+				restrict: 'E',
+				templateUrl: 'directives/taskView.html'
+			};
 		});
 	}
 )();
